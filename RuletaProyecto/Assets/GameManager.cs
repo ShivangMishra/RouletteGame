341d3e136966d4d6e53d174dcf521409a4a97f61
juacarchi <cardenasfanoce@gmail.com>
2022-07-23 11:56:18 +0200

    SelectorNew

--- RuletaProyecto/Assets/GameManager.cs
+++ RuletaProyecto/Assets/GameManager.cs
@@ -7,7 +7,10 @@ public class GameManager : MonoBehaviour
     public int numberSpins=1;
     public static GameManager instance;
     public float speedA=-300, speedB=-300, speedC=-300, speedD=-300;
+    public float speed2A = -300, speed2B = -300, speed2C = -300, speed2D = -300;
     public float timeA, timeB, timeC, timeD;
+    public float time2A, time2B, time2C, time2D;
+    public GameObject[] buttonsCustomize;
     private void Awake()
     {
         if (instance == null)
@@ -23,7 +26,37 @@ public class GameManager : MonoBehaviour
     public void SetNumberSpins(int i)
     {
         numberSpins = i;
+        switch (i)
+        { //CAN CODE BETTER
+            case 0:
+                buttonsCustomize[0].SetActive(true);
+                buttonsCustomize[1].SetActive(false);
+                buttonsCustomize[2].SetActive(false);
+                buttonsCustomize[3].SetActive(false);
+                break;
+
+            case 1:
+                buttonsCustomize[0].SetActive(true);
+                buttonsCustomize[1].SetActive(true);
+                buttonsCustomize[2].SetActive(false);
+                buttonsCustomize[3].SetActive(false);
+                break;
+
+            case 2:
+                buttonsCustomize[0].SetActive(true);
+                buttonsCustomize[1].SetActive(true);
+                buttonsCustomize[2].SetActive(true);
+                buttonsCustomize[3].SetActive(false);
+                break;
+            case 3:
+                buttonsCustomize[0].SetActive(true);
+                buttonsCustomize[1].SetActive(true);
+                buttonsCustomize[2].SetActive(true);
+                buttonsCustomize[3].SetActive(true);
+                break;
+        }
     }
+    #region 1st WinningNumber
     public void SetSpeedA(float f)
     {
         speedA = f;
@@ -57,4 +90,40 @@ public class GameManager : MonoBehaviour
     {
         timeD = f;
     }
+    #endregion
+    #region 2nd WinningNumber
+    public void SetSpeed2A(float f)
+    {
+        speed2A = f;
+    }
+    public void SetSpeed2B(float f)
+    {
+        speed2B = f;
+    }
+    public void SetSpeed2C(float f)
+    {
+        speed2C = f;
+    }
+    public void SetSpeed2D(float f)
+    {
+        speed2D = f;
+    }
+
+    public void SetTime2A(float f)
+    {
+        time2A = f;
+    }
+    public void SetTime2B(float f)
+    {
+        time2B = f;
+    }
+    public void SetTime2C(float f)
+    {
+        time2C = f;
+    }
+    public void SetTime2D(float f)
+    {
+        time2D = f;
+    }
+    #endregion
 }
